1c93fc04d23c3517f6923484d6ee2ef6ab4f4d88
Jose <espinal0599@gmail.com>
2026-04-01 11:26:32 -0400

    feat: reforzar la presentacion visual del dashboard y la vista de reportes.

--- Views/home/home.php
+++ Views/home/home.php
@@ -5,7 +5,7 @@
     <meta name='viewport' content='width=device-width,initial-scale=1.0'>  
     <title>NexoTI - Inicio</title>
     <link rel='icon' type='image/svg+xml' href='/NexoTI/favicon.svg'>  
-    <link rel='stylesheet' href='/NexoTI/Views/home/home.css'>  
+    <link rel='stylesheet' href='/NexoTI/Views/home/home.css?v=2'>  
     <link rel='stylesheet' href='/NexoTI/Views/partials/sidebar.css'>
     <link rel='stylesheet' href='/NexoTI/Views/partials/buttons.css'>
  <link rel='stylesheet' href='/NexoTI/Views/partials/app-shell.css'>
--- Views/reportes/reportes.php
+++ Views/reportes/reportes.php
@@ -5,7 +5,7 @@
     <meta name='viewport' content='width=device-width,initial-scale=1.0'>
     <title>NexoTI - Reportes</title>
     <link rel='icon' type='image/svg+xml' href='/NexoTI/favicon.svg'>
-    <link rel='stylesheet' href='/NexoTI/Views/reportes/reportes.css?v=3'>
+    <link rel='stylesheet' href='/NexoTI/Views/reportes/reportes.css?v=4'>
     <link rel='stylesheet' href='/NexoTI/Views/partials/sidebar.css'>
     <link rel='stylesheet' href='/NexoTI/Views/partials/buttons.css'>
  <link rel='stylesheet' href='/NexoTI/Views/partials/app-shell.css'>
